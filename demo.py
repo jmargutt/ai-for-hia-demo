4680fa973c56433acae2e29a7ffae4984619445b
jmargutti <jacopo.margutti@gmail.com>
2023-08-28 17:38:51 +0200

    bug fix

--- demo.py
+++ demo.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from test_langchain import langchain_search, langchain_question_answering
 
